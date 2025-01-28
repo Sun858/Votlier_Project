5b597fd25b130cdca7afd6f7ff0f1ea4a6eea0cb
Babadook1090 <59915647+Babadook1090@users.noreply.github.com>
2025-01-28 21:38:49 +1100

    Update to config file, creating the php for registration and functions
    As per the title.

--- Project-root/DatabaseConnection/config.php
+++ Project-root/DatabaseConnection/config.php
@@ -1,10 +1,8 @@
 <?php
 // Database connection config
-$con = mysqli_connect("localhost", "root", "", "voting_system"); // Add an empty string for the password (default is empty in XAMPP)
+$conn = mysqli_connect("localhost", "root", "", "voting_system");
 if (mysqli_connect_errno()) {
-    echo "Failed to connect to Database: " . mysqli_connect_error();
-} else {
-    echo "<p>Connection Established </p>";
+    die("Failed to connect to Database: " . mysqli_connect_error());
 }
 ?>
 
---
+++ Project-root/DatabaseConnection/submit-form.php
@@ -0,0 +1,46 @@
+<?php
+require 'config.php'; // This is the database connection including the authentication details
+require 'functions.sn.php'; // This has most of the functions that are called within this file
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') { // If the server request is UNIVERSALLY 'Post' then this will happen:
+    // Grab all the data that user has inputted into the FORM
+    $firstName = $_POST['first-name'];
+    $middleName = $_POST['middle-name'];
+    $lastName = $_POST['last-name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirm-password'];
+
+    // Input Validation
+    if (emptyInputSignup($firstName, $lastName, $email, $password, $confirmPassword) !== false) {
+        header("location: ../pages/signup.html?error=emptyinput");
+        exit();
+    }
+
+    // Email verification (valid)
+    if (invalidEmail($email) !== false) {
+        header("location: ../pages/signup.html?error=invalidemail");
+        exit();
+    }
+
+    // Password Verification
+    if (pwdMatch($password, $confirmPassword) !== false) {
+        header("location: ../pages/signup.html?error=passwordsdontmatch");
+        exit();
+    }
+
+    // This function will check the database if the email already exists and stop the user from creating a duplicate
+    if (emailExists($conn, $email) !== false) {
+        header("location: ../pages/signup.html?error=userexists");
+        exit();
+    }
+    
+
+
+
+
+
+    // Store all the PII (ENCRYPTED) and hashed password into the database
+    createUser($conn, $firstName, $middleName, $lastName, $email, $password, $confirmPassword);
+
+}
