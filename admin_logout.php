ee3b3e0ffca7ca26c953ecf779ba15de91caeaf0
Sharky-Codes <sharmarkefarah7@gmail.com>
2025-05-28 19:34:14 +1000

    admin_logout.php
    This file logs the admin out by ending the session and clearing their login data. Once successfully logged out It then redirects them back to the login page and denies access to anything until logged in again to keep things secure.

---
+++ admin_logout.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+// Terminates the session itself
+session_unset();
+session_destroy();
+
+// Once successfully logged out Redirects admin to the login page
+header("Location: admin_login.php");
+exit;
+?>
