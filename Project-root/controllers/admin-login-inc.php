93730268cc6581ca5f2b26a153e92e10a95f49d6
Babadook1090 <59915647+Babadook1090@users.noreply.github.com>
2025-08-16 22:10:09 +1000

    Directory Changes
    Need to change directory require once fields in the files

---
+++ Project-root/controllers/admin-login-inc.php
@@ -0,0 +1,38 @@
+<?php
+// This page handles the admin login functionality
+// It checks if the admin is already logged in, and if not, it processes the login.
+session_start();
+
+require_once '../DatabaseConnection/config.php';
+require_once 'security.sn.php';
+require_once 'functions.sn.php';
+
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$resource = 'admin_login';
+
+if (isRateLimitedDB($conn, $ip, $resource, 4, 900)) {
+    header("location: ../pages/stafflogin.php?error=ratelimited");
+    exit();
+}
+
+// Once again logs the attempt so they cant just refresh the page, or the DB loses track of the attempts.
+
+recordLoginAttemptDB($conn, $ip, $resource);
+
+
+
+if (isset($_POST["submit"])) {
+    $email = $_POST["loginEmail"];
+    $pwd = $_POST["loginPassword"];
+
+    if (emptyInputLogin($email, $pwd) !== false) {
+        header("location: ../pages/stafflogin.php?error=emptyinput");
+        exit();
+    }
+
+    loginAdmin($conn, $email, $pwd);
+} else {
+    header("location: ../pages/stafflogin.php");
+    exit();
+}
